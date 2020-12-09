4a57d51b70bcdc2003c7d630e083f75be3dcca6b
Nathan Smith <nathan@nfsmith.ca>
2020-12-09 15:03:16 -0800

    Put kubernetes provider back
    Oops. Looks like that stuff was actually being used.

--- eks/cluster.tf
+++ eks/cluster.tf
@@ -3,6 +3,21 @@ locals {
   k8s_version  = "1.18"
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks-cluster.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
 module "eks-cluster" {
   version                         = "13.0.0"
   source                          = "terraform-aws-modules/eks/aws"
--- eks/versions.tf
+++ eks/versions.tf
@@ -4,5 +4,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 1.9"
+    }
   }
 }
