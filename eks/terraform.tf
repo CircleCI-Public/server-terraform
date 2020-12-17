11d6834923638acc2c15c5ad090b252d34814c83
Chris Johnson <christopher@circleci.com>
2020-12-17 09:54:06 -0600

    use variables directly, put privider versions in requried_providers block

--- eks/terraform.tf
+++ eks/terraform.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      version = "~> 1.11"
+    }
+    aws        = {
+      version = "~> 3.0.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 3.0.0"
   region  = var.aws_region
   profile = var.aws_profile != "" ? var.aws_profile : null
 }
