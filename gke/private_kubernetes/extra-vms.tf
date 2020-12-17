8652e3a23f5f9e0c58dbca2b2e96954c0f29c850
Robin Sturmeit <robin.sturmeit@circleci.com>
2020-12-17 15:53:24 +0000

    Update bastion startup script

--- gke/private_kubernetes/extra-vms.tf
+++ gke/private_kubernetes/extra-vms.tf
@@ -54,7 +54,7 @@ resource "google_compute_instance" "bastion" {
 
   metadata_startup_script = join("\n", [
     "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.14.10/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/bin/",
-    "echo \"KUBECONFIG=/etc/kube.config gcloud container clusters get-credentials --internal-ip --region ${var.location} ${var.unique_name}-k8s-cluster && ( [ -e /home/ubuntu/.config ] && sudo chown -R ubuntu /home/ubuntu/.config ) && sudo chmod 0644 /etc/kube.config \" > update-kubeconfig && chmod +x update-kubeconfig && sudo mv ./update-kubeconfig /usr/bin/update-kubeconfig && update-kubeconfig",
+    "echo \"gcloud container clusters get-credentials --internal-ip --region ${var.location} ${var.unique_name}-k8s-cluster\" > update-kubeconfig && chmod +x update-kubeconfig && sudo mv ./update-kubeconfig /usr/bin/update-kubeconfig && update-kubeconfig",
     "curl -LO https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.5.4/kustomize_v3.5.4_linux_amd64.tar.gz && tar xzf ./kustomize_v3.5.4_linux_amd64.tar.gz && sudo mv ./kustomize /usr/bin/",
     "curl -LO https://github.com/replicatedhq/kots/releases/download/v1.24.1/kots_linux_amd64.tar.gz && tar xzf kots_linux_amd64.tar.gz && sudo mv ./kots /usr/bin/kubectl-kots",
     "curl -LO https://github.com/replicatedhq/troubleshoot/releases/download/v0.9.51/preflight_linux_amd64.tar.gz && tar xzf preflight_linux_amd64.tar.gz && sudo mv ./preflight /usr/bin/kubectl-preflight"
