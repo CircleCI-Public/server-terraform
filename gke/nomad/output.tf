8896a84ebfdceffa0a430a05520201db040036e5
Nathan Smith <nathan@nfsmith.ca>
2021-01-04 09:49:57 -0800

    Use index to reference nomad_tls module

--- gke/nomad/output.tf
+++ gke/nomad/output.tf
@@ -1,11 +1,11 @@
 output "nomad_server_cert" {
-  value = module.nomad_tls ? module.nomad_tls.nomad_server_cert : ""
+  value = length(module.nomad_tls) == 1 ? module.nomad_tls[0].nomad_server_cert : ""
 }
 
 output "nomad_server_key" {
-  value = module.nomad_tls ? module.nomad_tls.nomad_server_key : ""
+  value = length(module.nomad_tls) == 1 ? module.nomad_tls[0].nomad_server_key : ""
 }
 
 output "nomad_tls_ca" {
-  value = module.nomad_tls ? module.nomad_tls.nomad_tls_ca : ""
+  value = length(module.nomad_tls) == 1 ? module.nomad_tls[0].nomad_tls_ca : ""
 }
