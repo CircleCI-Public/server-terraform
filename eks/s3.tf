7ccd00bad0da5d9a90dd79f5af0a31d601911559
soulchips <akil@circleci.com>
2021-01-07 10:18:56 -0500

    switching s3 to default encryption

--- eks/s3.tf
+++ eks/s3.tf
@@ -1,43 +1,3 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_kms_key" "data_bucket_key" {
-  description             = "This key is used to encrypt the data_bucket"
-  deletion_window_in_days = 10
-
-
-  # This policy allows the root to manage the keys - needed for terraform to manage the keys and provides the cluster nodes with limited access to keys
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "kms:*"
-      ],
-      "Principal": { "AWS": [
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        ]
-      },
-      "Effect": "Allow",
-      "Resource": "*"
-    },
-    {
-      "Action": [
-        "kms:Decrypt",
-        "kms:GenerateDataKey*"
-      ],
-      "Principal": { "AWS": [
-          "${module.eks-cluster.worker_iam_role_arn}"
-        ]
-      },
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
 resource "aws_s3_bucket" "data_bucket" {
   bucket        = "${var.basename}-data"
   force_destroy = var.force_destroy
@@ -47,12 +7,11 @@ resource "aws_s3_bucket" "data_bucket" {
     allowed_origins = ["*"]
     max_age_seconds = 3600
   }
-
+  
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.data_bucket_key.arn
-        sse_algorithm     = "aws:kms"
+        sse_algorithm = "AES256"
       }
     }
   }
