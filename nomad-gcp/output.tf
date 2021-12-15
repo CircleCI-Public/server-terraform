d5bd932666e9a08c1d5eae0d5dce72fdaea61c20
soulchips <akil@circleci.com>
2021-12-14 21:05:29 -0500

    improving output for gcp

--- nomad-gcp/output.tf
+++ nomad-gcp/output.tf
@@ -9,3 +9,15 @@ output "nomad_server_key" {
 output "nomad_tls_ca" {
   value = var.unsafe_disable_mtls ? "" : module.tls[0].nomad_tls_ca
 }
+
+output "managed_instance_group_name" {
+  value = google_compute_instance_group_manager.nomad.name
+}
+
+output "managed_instance_group_type" {
+  value = "zonal"
+}
+
+output "service_account_key_location" {
+  value = "${path.cwd}/nomad-as-key.json"
+}
