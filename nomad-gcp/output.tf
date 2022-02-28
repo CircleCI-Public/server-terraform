4ea68b96b63f21383a8a032cb463c19308f484f0
Atul Singh <atul@circleci.com>
2022-02-28 17:34:31 -0500

    server-1557 | added gco zone in tf output

--- nomad-gcp/output.tf
+++ nomad-gcp/output.tf
@@ -18,6 +18,14 @@ output "managed_instance_group_type" {
   value = "zonal"
 }
 
+output "managed_instance_group_region" {
+  value = google_compute_target_pool.nomad.region
+}
+
+output "managed_instance_group_zone" {
+  value = google_compute_instance_group_manager.nomad.zone
+}
+
 output "service_account_key_location" {
   value = var.enable_workload_identity  ? "" : "${path.cwd}/nomad-as-key.json"
 }
