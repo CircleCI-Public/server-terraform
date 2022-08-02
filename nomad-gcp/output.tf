d7b50232bdaf9e4ac36514905d5b4d3a45d1ae98
circlery <104859301+circlery@users.noreply.github.com>
2022-08-02 11:16:12 -0400

    GCP Nomad Client - Additional Project Support and Key Output (#159)
    * supporting custom project id var, outputting sa json
    
    * formatting tf
    
    * outputting key only when necessary
    
    * fixing typo
    
    * formatting tf

--- nomad-gcp/output.tf
+++ nomad-gcp/output.tf
@@ -38,6 +38,12 @@ output "managed_instance_group_zone" {
   value = google_compute_instance_group_manager.nomad.zone
 }
 
+output "service_account_key" {
+  value       = local.output_sa_key ? base64decode(google_service_account_key.nomad-as-key[0].private_key) : ""
+  sensitive   = true
+  description = "Base64 decoded service account key."
+}
+
 output "service_account_key_location" {
   value = var.enable_workload_identity ? "" : "${path.cwd}/nomad-as-key.json"
 }
