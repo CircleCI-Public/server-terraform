00135e52335eeeb4b1696a5a9593363b310d0ae9
Ryder <ryder@circleci.com>
2023-05-10 15:55:31 -0400

    testing terraform version and provider version bump

--- nomad-aws/examples/basic/main.tf
+++ nomad-aws/examples/basic/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">=0.15.2"
+  required_version = ">=1.4.6"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>3.0"
+      version = "~>4.66"
     }
   }
 }
