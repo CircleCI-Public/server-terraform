e25a983b0ed020961ebb6e3e7b72070b27bb26a0
Atul S <3430610+atulsingh0@users.noreply.github.com>
2025-05-26 10:17:37 -0400

    ONPREM-2249 | Restrict nomad traffic within vpc for AWS Nomad (#218)
    * restrict nomad traffic with vpc for aws
    
    update aws vpc provider
    
    * aws doc update
    
    * make docs
    
    ---------
    
    Co-authored-by: Chris Stephen <chris.stephen@circleci.com>

--- nomad-aws/security_groups.tf
+++ nomad-aws/security_groups.tf
@@ -20,6 +20,28 @@ resource "aws_security_group" "nomad_sg" {
   }
 }
 
+resource "aws_security_group" "nomad_traffic_sg" {
+  name        = "${var.basename}-nomad_traffic_sg"
+  description = "Security group that restrict Nomad Client Server communication within VPC CIDR"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "Allow CircleCI Nomad traffic within VPC"
+    from_port   = 4646
+    to_port     = 4648
+    protocol    = "tcp"
+    cidr_blocks = data.aws_vpc.nomad.cidr_block
+  }
+
+  egress {
+    description = "Allow CircleCI Nomad outbound connections"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # tfsec:ignore:aws-vpc-no-public-egress-sgr
+  }
+}
+
 resource "aws_security_group" "ssh_sg" {
   description = "Security group that permits CircleCI Server to SSH into nomad instances"
   count       = var.ssh_key != null ? 1 : 0
